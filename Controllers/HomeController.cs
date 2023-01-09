ee9a2d6dfc20c3ac82bb33ad4cd13be90f9f87b6
Abhishek Talati <abhishek.talati91@outlook.com>
2023-01-09 19:26:23 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication3.Models;
+using WebApplication3.Service;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public readonly EmployeeService _employeeService;
+        public readonly IConfiguration _IConfig;
+        public HomeController(EmployeeService _employeeService , IConfiguration _IConfig)
+        {
+            this._employeeService = _employeeService;
+            this._IConfig = _IConfig;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Employee> employee = _employeeService.GetEmployee(_IConfig.GetConnectionString("SQLConnection"));
+
+            return View(employee);
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Service/EmployeeService.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+using WebApplication3.Models;
+
+namespace WebApplication3.Service
+{
+    public class EmployeeService
+    {
+        private SqlConnection GetConnection(string _connectionStrimg)
+        {
+            return new SqlConnection(_connectionStrimg);
+
+        }
+
+        public IEnumerable<Employee> GetEmployee(string _connectionStrimg)
+        {
+            List<Employee> employees = new List<Employee>();
+            string str = "select * from dbo.Test";
+
+            SqlConnection con = GetConnection(_connectionStrimg);
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand(str, con);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Employee employee = new Employee()
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Phone = reader.GetString(2)
+
+                    };
+                    employees.Add(employee);
+                }
+            }
+            con.Close();
+
+            return employees;
+
+        }
+    }
+}
